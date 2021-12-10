1eed52c4b8da1d705a8273df7e6285c683608b41
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2021-12-10 15:07:46 +0200

    variables.js

--- js/variables.js
+++ js/variables.js
@@ -5,12 +5,24 @@ console.log(student);
 student = 'Vika';
 console.log(student);
 
+var count;
+console.log(count);
+count = 26;
+console.log(count);
+// переменную var можно объявлять без инициализации и потом присваивать значение
+
 //console.log(number); //нельзя выводить в консоль до объявления
 let number = 30;
 console.log(number);
 // у переменной  let  можно менять значение
 number = 560;
 console.log(number);
+
+let age;
+console.log(age);
+age = 18;
+console.log(age);
+// переменную let можно объявлять без инициализации и потом присваивать значение
 
 // const нельзя выводить в консоль до объявления
 const userName = 'Vova';
