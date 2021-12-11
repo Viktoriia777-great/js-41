eee3fcceec34f4ba7a59240f8776f8fbfdfc479b
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2021-12-11 18:28:05 +0200

    false-variables.js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 // import './js/primitives.js';
 //import './js/toString.js';
 //import './js/toNumber.js';
-import './js/boolean.js';
+//import './js/boolean.js';
+import './js/false-variables.js';
