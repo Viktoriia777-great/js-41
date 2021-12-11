45910ca83ecbbcdce1ff8bb7ee2acb703d14f60f
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2021-12-11 17:23:04 +0200

    toNumber.js

--- js/toNumber.js
+++ js/toNumber.js
@@ -7,7 +7,146 @@ let testString = 'Bla Bla Bla';
 console.log(testString);
 console.log(typeof testString);
 
-// КОНСТРУКТОР Number()
+// 1-й способ КОНСТРУКТОР Number()
 let toNumber = Number(testString);
 console.log(`${testString}, приведение строки к числу через конструктор Number():`, toNumber);
 console.log(`тип данных ${testString}, через конструктор Number():`, typeof toNumber);
+
+// 2-й способ УНАРНЫЙ +
+toNumber = +testString;
+console.log(`${testString}, приведение строки к числу через унарный + :`, toNumber);
+console.log(`тип данных ${testString}, через унарный +`, typeof toNumber);
+
+// строчное число к числу
+testString = '123';
+console.log(testString);
+console.log(typeof testString);
+
+toNumber = Number(testString);
+console.log(
+  `${testString}, приведение строчного числа к числу через конструктор Number():`,
+  toNumber,
+);
+console.log(`тип данных ${testString}, через конструктор Number():`, typeof toNumber);
+
+toNumber = +testString;
+console.log(`${testString}, приведение строчного числа к числу через унарный + :`, toNumber);
+console.log(`тип данных ${testString}, через унарный +`, typeof toNumber);
+
+// ПУСТАЯ СТРОКА
+testString = '';
+console.log(testString);
+console.log(typeof testString);
+
+toNumber = Number(testString);
+console.log(
+  `ПУСТАЯ СТРОКА${testString}, приведение строки к числу через конструктор Number():`,
+  toNumber,
+);
+console.log(`тип данных ПУСТОЙ СТРОКИ ${testString}, через конструктор Number():`, typeof toNumber);
+
+toNumber = +testString;
+console.log(`ПУСТАЯ СТРОКА ${testString}, приведение строки к числу через унарный + :`, toNumber);
+console.log(`тип данных ПУСТОЙ СТРОКИ ${testString}, через унарный +`, typeof toNumber);
+
+// СТРОКА С ПРОБЕЛОМ
+testString = '  ';
+console.log(testString);
+console.log(typeof testString);
+
+toNumber = Number(testString);
+console.log(
+  `СТРОКА С ПРОБЕЛОМ ${testString}, приведение строки к числу через конструктор Number():`,
+  toNumber,
+);
+console.log(
+  `тип данных СТРОКИ С ПРОБЕЛОМ ${testString}, через конструктор Number():`,
+  typeof toNumber,
+);
+
+toNumber = +testString;
+console.log(
+  `СТРОКА С ПРОБЕЛОМ ${testString}, приведение строки к числу через унарный + :`,
+  toNumber,
+);
+console.log(`тип данных СТРОКИ С ПРОБЕЛОМ ${testString}, через унарный +`, typeof toNumber);
+
+// BOOLEAN к числу
+testString = true;
+console.log(testString);
+console.log(typeof tesrString);
+
+toNumber = Number(testString);
+console.log(`${testString}, приведение BOOLEAN к числу через конструктор Number():`, toNumber);
+console.log(`тип данных ${testString}, через конструктор Number():`, typeof toNumber);
+
+toNumber = +testString;
+console.log(`${testString}, приведение BOOLEAN к числу через унарный +:`, toNumber);
+console.log(`тип данных ${testString}, через унарный +:`, typeof toNumber);
+
+testString = false;
+console.log(testString);
+console.log(typeof tesrString);
+
+toNumber = Number(testString);
+console.log(`${testString}, приведение BOOLEAN к числу через конструктор Number():`, toNumber);
+console.log(`тип данных ${testString}, через конструктор Number():`, typeof toNumber);
+
+toNumber = +testString;
+console.log(`${testString}, приведение BOOLEAN к числу через унарный +:`, toNumber);
+console.log(`тип данных ${testString}, через унарный +:`, typeof toNumber);
+
+// NaN
+testString = NaN;
+console.log(testString);
+console.log(typeof tesrString);
+
+toNumber = Number(testString);
+console.log(`${testString}, приведение NaN к числу через конструктор Number():`, toNumber);
+console.log(`тип данных ${testString}, через конструктор Number():`, typeof toNumber);
+
+toNumber = +testString;
+console.log(`${testString}, приведение NaN к числу через унарный +:`, toNumber);
+console.log(`тип данных ${testString}, через унарный +:`, typeof toNumber);
+
+// undefined
+testString = undefined;
+console.log(testString);
+console.log(typeof tesrString);
+
+toNumber = Number(testString);
+console.log(`${testString}, приведение UNDEFINED к числу через конструктор Number():`, toNumber);
+console.log(`тип данных ${testString}, через конструктор Number():`, typeof toNumber);
+
+toNumber = +testString;
+console.log(`${testString}, приведение UNDEFINED к числу через унарный +:`, toNumber);
+console.log(`тип данных ${testString}, через унарный +:`, typeof toNumber);
+
+// infinity
+testString = Infinity;
+console.log(testString);
+console.log(typeof tesrString);
+
+toNumber = Number(testString);
+console.log(`${testString}, приведение INFINITY к числу через конструктор Number():`, toNumber);
+console.log(`тип данных ${testString}, через конструктор Number():`, typeof toNumber);
+
+toNumber = +testString;
+console.log(`${testString}, приведение INFINITY к числу через унарный +:`, toNumber);
+console.log(`тип данных ${testString}, через унарный +:`, typeof toNumber);
+
+// ЧИСЛО С БУКВАМИ И СИМВОЛАМИ
+testString = '35px';
+console.log(testString);
+console.log(typeof testString);
+
+toNumber = Number(testString);
+console.log(
+  `${testString}, приведение строчного числа к числу через конструктор Number():`,
+  toNumber,
+);
+console.log(`тип данных ${testString}, через конструктор Number():`, typeof toNumber);
+
+toNumber = +testString;
+console.log(`${testString}, приведение строчного числа к числу через унарный + :`, toNumber);
+console.log(`тип данных ${testString}, через унарный +`, typeof toNumber);
