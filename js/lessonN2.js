904d53ec6d01bec5ed1d52813390c4bfc4ba31e4
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2022-01-02 20:33:41 +0200

    сумма чисел в отрезке

--- js/lessonN2.js
+++ js/lessonN2.js
@@ -113,6 +113,43 @@ switch (option) {
 
 console.log(message);
 
-for (let i = 0; i < 10; i += 1) {
+for (let i = 0; i <= 10; i += 1) {
   console.log(i);
 }
+/*
+ * Напиши скрипт который подсчитывает общую сумму зарплат работников.
+ * Кол-во работников хранится в перемнной employees.
+ * Зарплата каждого работника это случайное число от 500 до 5000
+ * Записать сумму в переменную totalSalary и вывести в консоль
+ */
+const employees = 8;
+const minSalary = 500;
+const maxSalary = 5000;
+let totalSalary = 0;
+
+for (let i = 1; i <= employees; i += 1) {
+  const salary = Math.round(Math.random() * (maxSalary - minSalary) + minSalary);
+  console.log(`ЗП работника номер ${i} - ${salary}`);
+
+  totalSalary += salary;
+}
+
+console.log('totalSalary:', totalSalary);
+
+/*
+ * Напиши скрипт который подсчитывает сумму всех чётных чисел,
+ * которые входят диапазон чисел в переменных от min до max.
+ * Например, если min=0 и max=5, то диапазон 0-5, и в нём два чётных числа - 2 и 4, их сумма 6.
+ */
+const min = 0;
+const max = 5;
+let total = 0;
+
+for (let i = min; i <= max; i += 1) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log('Четное:', i);
+  total += i;
+}
+console.log(`Сумма четніх чисел в отрезке от ${min} до ${max}:`, total);
