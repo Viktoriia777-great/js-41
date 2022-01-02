647e536b18e5adb7f3eee86ad8ce7713d36f95ef
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2022-01-02 16:05:20 +0200

    задачи

--- js/lessonN2.js
+++ js/lessonN2.js
@@ -42,3 +42,77 @@ switch (stars) {
     console.log('Такого лоличества звезд нет');
 }
 console.log(price);
+
+// скрипт вибора стоимости отеля по кол-ву звезд
+// 1,2 - 20; 3,4 - 30; 5 - 120
+
+const starss = 3;
+let prices;
+
+if (starss === 1 || starss === 2) {
+  prices = 20;
+} else if (starss === 3 || starss === 4) {
+  prices = 30;
+} else if (starss === 5) {
+  prices = 120;
+} else {
+  console.log('Такого лоличества звезд нет');
+}
+
+console.log(prices);
+
+switch (starss) {
+  case 1:
+  case 2:
+    prices = 50;
+    break;
+
+  case 3:
+  case 4:
+    prices = 70;
+    break;
+
+  case 5:
+    prices = 120;
+    break;
+  default:
+    console.log('Такого лоличества звезд нет');
+}
+
+console.log(prices);
+
+/*
+ * Напиши скрипт выбора опции доставки товара.
+ * Опция хранится в переменной option: 1 - самовывоз, 2 - курьер, 3 - почта
+ *
+ * В переменную message записать сообщение в зависимости от опции.
+ * - 'Вы сможете забрать товар завтра с 12:00 в нашем офисе'
+ * - 'Курьер доставит заказ завтра с 9:00 до 18:00'
+ * - 'Посылка будет отправлена сегодня'
+ * - 'Вам перезвонит менеджер'
+ */
+
+const option = 6;
+let message;
+
+switch (option) {
+  case 1:
+    message = 'Вы сможете забрать товар завтра с 12:00 в нашем офисе';
+    break;
+
+  case 2:
+    message = 'Курьер доставит заказ завтра с 9:00 до 18:00';
+    break;
+  case 3:
+    message = 'Посылка будет отправлена сегодня';
+    break;
+
+  default:
+    console.log('Вам перезвонит менеджер');
+}
+
+console.log(message);
+
+for (let i = 0; i < 10; i += 1) {
+  console.log(i);
+}
