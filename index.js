ad26b423bbf2735b651d4e0e6d598a5c02d7e77c
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2022-01-04 16:09:33 +0200

    correction

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 //import './js/toNumber.js';
 //import './js/boolean.js';
 //import './js/false-variables.js';
-import './js/modul1.1.js';
-import './js/branching-operators.js';
-import './js/lessonN2.js';
-import './js/lessonN3.js';
+//import './js/modul1.1.js';
+//import './js/branching-operators.js';
+//import './js/modul1.2.js';
+import './js/modul2.1.js';
