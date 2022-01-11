61559c47c10921bc341caee69145358dea170937
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2022-01-11 21:29:15 +0200

    решает задачи массив

--- js/modul2.1-tasks.js
+++ js/modul2.1-tasks.js
@@ -28,15 +28,23 @@ console.log('total:', total);*/
 // 1 найти четные
 // 2 вычислить тотал
 // вывести в консоль
-const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
-let total = 0;
+//const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+//let total = 0;
 
-for (const number of numbers) {
-  if (number % 2 === 0) {
-    total += number;
-  }
-}
-console.log('Total:', total);
+//for (const number of numbers) {
+//if (number % 2 === 0) {
+// total += number;
+//}
+//}
+
+// for (const number of numbers) {
+//   if (number % 2 !== 0) {
+//     continue;
+//   }
+//   total += number;
+// }
+
+//console.log('Total:', total);
 /*
  * Напиши скрипт поиска логина
  * - Если логина нет, вывести сообщение 'Пользователь [логин] не найден.'
@@ -49,35 +57,35 @@ console.log('Total:', total);
  */
 
 const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
-const loginToFind = 'j4xth3m4n';
+const loginToFind = 'k1widab3st';
 let massage;
 
 // 1 перебрать массив
-// сравнить логины
-// переменная massage
-// вывести сообщения в консоль
+//2 сравнить логины
+// 3 переменная massage
+//4  вывести сообщения в консоль
 
 // for
-// for (let i = 0; i < logins.length; i += 1) {
-//   console.log(logins[i]);
-//   if (loginToFind === logins[i]) {
-//     massage = `Пользователь ${loginToFind} найден.`;
-//     break;
-//   }
-//   massage = `Пользователь ${loginToFind} не найден.`;
-// }
-// console.log(massage);
+//for (let i = 0; i < logins.length; i += 1) {
+// console.log(logins[i]);
+// if (loginToFind === logins[i]) {
+//  massage = `Пользователь ${loginToFind} найден.`;
+//  break;
+//}
+// massage = `Пользователь ${loginToFind} не найден.`;
+//}
+//console.log(massage);
 
 // for of
 
-// for (const login of logins) {
-//   if (loginToFind === login) {
-//     massage = `Пользователь ${loginToFind} найден.`;
-//     break;
-//   }
-//   massage = massage = `Пользователь ${loginToFind} не найден.`;
+//for (const login of logins) {
+// if (loginToFind === login) {
+//   massage = `Пользователь ${loginToFind} найден.`;
+//   break;
 // }
-// console.log(massage);
+// massage = massage = `Пользователь ${loginToFind} не найден.`;
+//}
+//console.log(massage);
 
 //Метод includes() с тернарным оператором
 //const type = age >= 18 ? 'adult' : 'child';
@@ -89,3 +97,36 @@ massage = logins.includes(loginToFind)
   ? `Пользователь ${loginToFind} найден.`
   : `Пользователь ${loginToFind} не найден.`;
 console.log(massage);
+
+let a = 10;
+let b = a;
+console.log(a);
+console.log(b);
+
+a = 20;
+//b = a;
+console.log(a);
+console.log(b);
+
+/*
+ * Напиши скрипт поиска самого маленького числа в массиве,
+ * при условии что числа уникальные (не повторяются).
+ */
+
+const numbers = [51, 18, 13, 24, 7, 85, 19];
+let smallNumber = numbers[0];
+let bigNumber = numbers[0];
+
+for (const number of numbers) {
+  if (number < smallNumber) {
+    smallNumber = number;
+  }
+}
+console.log('Smallnumber:', smallNumber);
+
+for (const number of numbers) {
+  if (number > bigNumber) {
+    bigNumber = number;
+  }
+}
+console.log('Bignumber:', bigNumber);
