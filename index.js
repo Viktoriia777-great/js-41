8e1b85cc99b98d3783d07f6c26a126e78e3b5c7b
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2022-01-16 17:22:13 +0200

    Модуль 2- Функция

--- index.js
+++ index.js
@@ -9,4 +9,5 @@
 //import './js/branching-operators.js';
 //import './js/modul1.2.js';
 //import './js/modul2.1.js';
-import './js/modul2.1-tasks.js';
+//import './js/modul2.1-tasks.js';
+import './js/modul2.2.js';
