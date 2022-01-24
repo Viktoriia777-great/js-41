027cbca3280c23b37da92d7b726d86c5cb22f5d5
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2022-01-24 20:33:13 +0200

    перебор объекта

--- js/modul3.1.js
+++ js/modul3.1.js
@@ -166,3 +166,181 @@ console.log(playlist.getTrackCount());
 playlist.updateRating(4);
 
 console.log(playlist);
+/*
+ * Перебор через for...in и Object.keys|values|entries
+ */
+// Примери с конспекта
+/*const hotel = {
+  name: 'Resort Hotel',
+  stars: 5,
+  capacity: 100,
+};
+
+const keys = Object.keys(hotel); // ["name", "stars", "capacity"]
+const values = Object.values(hotel); // ["Resort Hotel", 5, 100]
+const entries = Object.entries(hotel); // [Array(2), Array(2), Array(2)]
+console.log(hotel);
+console.log(keys);
+console.log(values);
+console.log(entries);*/
+const hotel = {
+  name: 'Resort Hotel',
+  stars: 5,
+  capacity: 100,
+};
+
+/*
+ * Посмотрите в консоли из каких двух элементов состоит каждый подмассив.
+ * Первый элемент это ключ свойства, второй это значение.
+ */
+const entries = Object.entries(hotel);
+
+/*
+ * На каждой итерации по entries выберем первый элемент подмассива (ключ)
+ * в переменную key, а второй (значение) в переменную value
+ */
+for (const entry of entries) {
+  const key = entry[0];
+  const value = entry[1];
+
+  console.log(`${key}: ${value}`);
+}
+
+/*
+ * name: Resort Hotel
+ * stars: 5
+ * capacity: 100
+ */
+
+// Примеры с видео Репеты
+const feedback = {
+  good: 5,
+  neutral: 10,
+  bad: 3,
+};
+
+let totalFeedback = 0;
+
+const keys = Object.keys(feedback);
+
+console.log(keys);
+
+/*for (const key of keys) {
+  console.log(key);
+  console.log(feedback[key]);
+
+  totalFeedback += feedback[key];
+}
+
+console.log('totalFeedback: ', totalFeedback);*/
+
+const values = Object.values(feedback);
+
+console.log(values);
+
+for (const value of values) {
+  console.log(value);
+
+  totalFeedback += value;
+}
+
+console.log('totalFeedback: ', totalFeedback);
+/*
+ * Работа с коллекцией (массивом объектов)
+ */
+const friends = [
+  { name: 'Mango', online: false },
+  { name: 'Kiwi', online: true },
+  { name: 'Poly', online: true },
+  { name: 'Ajax', online: false },
+];
+/*
+friends[0].newfriend = 777;
+friends[1].newfriend = 111;
+friends[2].newfriend = 222;
+friends[3].newfriend = 333;
+
+console.table(friends);
+for (const friend of friends) {
+  console.log(friend);
+  friend.newprop = 555;
+}*/
+console.table(friends);
+/*
+ * Ищем друга по имени
+ */
+const findFriendByName = function (allFriends, friendName) {
+  for (const friend of friends) {
+    console.log(friend.name);
+    if (friend.name === friendName) {
+      return 'Нашли :)';
+    }
+  }
+  return 'Нет такого имени :(';
+};
+console.log(findFriendByName(friends, 'Poly'));
+console.log(findFriendByName(friends, 'Chelsy'));
+/*
+ * Получаем имена всех друзей
+ */
+const getAllName = function (allFriends) {
+  const names = [];
+  for (const friend of allFriends) {
+    console.log(friend.name);
+    names.push(friend.name);
+  }
+  return names;
+};
+
+console.log(getAllName(friends));
+/*
+ * Получаем имена только друзей которые онлайн
+ */
+const getOnLineFriends = function (allFriends) {
+  const onLineFriends = [];
+
+  for (const friend of allFriends) {
+    console.log(friend);
+    console.log(friend.online);
+
+    if (friend.online) {
+      onLineFriends.push(friend);
+    }
+  }
+  return onLineFriends;
+};
+
+console.log(getOnLineFriends(friends));
+/*
+ * Получаем имена только друзей которые оffлайн
+ */
+const getOffLineFriends = function (allFriends) {
+  const offLineFriends = [];
+
+  for (const friend of allFriends) {
+    if (!friend.online) {
+      offLineFriends.push(friend);
+    }
+  }
+
+  return offLineFriends;
+};
+console.log(getOffLineFriends(friends));
+// создать 2 массива онлайн и офлайн?
+// если тру - в первый, если нет - во второй
+const getFriendByOnLineStatus = function (allFriends) {
+  const friendByStatus = {
+    online: [],
+    offline: [],
+  };
+  for (const friend of allFriends) {
+    if (friend.online) {
+      friendByStatus.online.push(friend);
+      continue;
+    }
+    friendByStatus.offline.push(friend);
+  }
+
+  return friendByStatus;
+};
+console.log(getFriendByOnLineStatus(friends));
