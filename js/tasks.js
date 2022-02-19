aae9732cf9ea3875deb616c03bba2317c1fe60c7
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2022-02-19 23:56:06 +0200

    Update tasks.js

--- js/tasks.js
+++ js/tasks.js
@@ -430,3 +430,23 @@ const logItems = function (array) {
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+/*Напиши скрипт подсчета стоимости гравировки украшений. Для этого создай функцию calculateEngravingPrice(message, pricePerWord) 
+принимающую строку (в строке будут только слова и пробелы) и цену гравировки одного слова, и возвращающую цену гравировки всех слов в строке.*/
+
+const calculateEngravingPrice = function (message, pricePerWord) {
+  const string = message.split(' ');
+  const prices = string.length * pricePerWord;
+  return prices;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
+
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
+
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
+
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
