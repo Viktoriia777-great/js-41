56f0b4b97c48e30d022bfaf4c3947992385a072f
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2022-02-20 16:07:12 +0200

    Update tasks.js

--- js/tasks.js
+++ js/tasks.js
@@ -519,3 +519,28 @@ console.log(formatString('Curabitur ligula sapien.', 40));
 
 console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.', 40));
 // вернется форматированная строка
+
+/*
+const formatString = function (string) {
+  const substring = string.slice(0, 40);
+
+  if (string.length > length) {
+    return substring + '...';
+  }
+  return substring;
+};
+
+
+  Вызовы функции для проверки работоспособности твоей реализации:
+ 
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
+
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
+
+console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'));
+// вернется форматированная строка*/
