5d68a2858c3b297032ef132ae728f0bcc053058a
Viktoriia777-great <84077745+Viktoriia777-great@users.noreply.github.com>
2022-02-20 15:46:29 +0200

    Update tasks.js

--- js/tasks.js
+++ js/tasks.js
@@ -450,3 +450,44 @@ console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient m
 console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
 
 console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
+/*Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку
+ (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.*/
+
+const findLongestWord = function (string) {
+  let arrString = string.split(' ');
+  let wordLength = 0;
+  let longestWord;
+  console.log(string);
+  for (let i = 0; i < arrString.length; i += 1) {
+    wordLength = arrString[1].length;
+    if (arrString[i].length > wordLength) {
+      longestWord = arrString[i];
+      return longestWord;
+    }
+  }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
+
+// const findLongestWord = function (str) {
+//   let arrStr = str.split(' ');
+//   let wordLength = 0;
+//   let longestWord;
+
+//   for (let i = 0; i < arrStr.length; i += 1) {
+//     wordLength = arrStr[1].length;
+
+//     if (arrStr[i].length > wordLength) {
+//       longestWord = arrStr[i];
+
+//       return longestWord;
+//     }
+//   }
+// };
